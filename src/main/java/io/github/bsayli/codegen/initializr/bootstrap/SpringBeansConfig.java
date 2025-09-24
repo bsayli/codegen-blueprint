ca273a02900607ac94113a8c86e7d3a209a06b07
bsayli <baris.sayli@gmail.com>
2025-09-24 17:28:21 -0600

    chore(config): remove unused MavenPomPort bean from SpringBeansConfig

--- src/main/java/io/github/bsayli/codegen/initializr/bootstrap/SpringBeansConfig.java
+++ src/main/java/io/github/bsayli/codegen/initializr/bootstrap/SpringBeansConfig.java
@@ -1,15 +1,8 @@
 package io.github.bsayli.codegen.initializr.bootstrap;
 
-import io.github.bsayli.codegen.initializr.adapter.out.StandardArtifactsAdapter;
-import io.github.bsayli.codegen.initializr.domain.port.out.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringBeansConfig {
-
-  @Bean
-  ProjectArtifactsPort projectArtifactsPort(MavenPomPort mavenPomPort) {
-    return new StandardArtifactsAdapter(mavenPomPort);
-  }
+  
 }
